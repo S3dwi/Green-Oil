28a2b8fb5db75d670c2ff591901bdc2d1d33d549
raefshah <113862946+raefshah@users.noreply.github.com>
2024-11-09 00:05:31 +0300

    adding colorScheme
    Update main.dark theme by adding colorScheme (all the possible color in the app)

--- green_oil/lib/main.dart
+++ green_oil/lib/main.dart
@@ -6,12 +6,32 @@ final theme = ThemeData(
   useMaterial3: true,
   scaffoldBackgroundColor: const Color(0xFFF8F8F8),
   cardColor: const Color(0xFFFFFFFF),
-  // Primary color used for elements like buttons and icon.
   primaryColor: const Color(0xFF47AB4D),
   disabledColor: const Color(0xFFA9A9AC),
   fontFamily: 'Lato',
-  shadowColor: Colors.black.withOpacity(0.9),
+  shadowColor: Colors.black.withOpacity(0.24),
+  buttonTheme: ButtonThemeData(
+    buttonColor: const Color(0xFF47AB4D),
+  ),
+  iconTheme: IconThemeData(
+    color: const Color(0xFF47AB4D), 
+  ),
 );
+
+final colorScheme = ColorScheme(
+  primary: Color(0xFF47AB4D), // Main color used for primary interactive elements
+  primaryContainer: Color(0xFF388E3C), // Variant of primary (lighter or darker), used for contrast on surfaces
+  secondary: Color(0xFF000000), // Accent color for secondary UI components
+  surface: Color(0xFFFFFFFF), // Color for surfaces like cards and sheets
+  error: Colors.red, // Error color for showing validation issues
+  
+  onPrimary: Colors.white, // Text/icon color on primary-colored backgrounds
+  onSecondary: Colors.white, // Text/icon color on secondary-colored backgrounds
+  onSurface: Colors.black, // Text/icon color on surface backgrounds
+  onError: Colors.white, // Text/icon color on error backgrounds
+  brightness: Brightness.light, // Theme brightness: light or dark
+);
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
